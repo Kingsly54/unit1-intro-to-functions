993039dfc68ae8f44a181e4117fc128dc4b78bc6
Kingsly54 <kingslyw@nycstudents.net>
2026-01-28 09:25:11 -0500

    unit1

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
-print("tests")
+import turtle
+from turtle import *
+t = Turtle()
+t.shape('turtle')
+
+def square(x):
+    t.forward(x)
+    t.left(90) 
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90) 
+    t.forward(x)
+    t.left(90) 
+def message(input): 
+    print(input)
+
+square(50)
+message("h")
+    
+turtle.done()
